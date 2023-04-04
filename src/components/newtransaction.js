ec00ddeb62ff90dec8cf79a55d9c0124498ccac3
vatsal-dev <chawlavatsal27@gmail.com>
2023-04-04 23:22:15 +0530

    Added tags feature for categorization

--- src/components/newtransaction.js
+++ src/components/newtransaction.js
@@ -1,27 +1,36 @@
 import React, { useState, useContext } from "react";
 import { GlobalContext } from "../context/GlobalStats";
 import "../App.css";
+import { WithContext as ReactTags } from "react-tag-input";
 
 export const Newtransaction = () => {
   const { addTransaction } = useContext(GlobalContext);
   const [text, setText] = useState("");
   const [amount, setAmount] = useState();
   const [type, setType] = useState("E");
-  const [person, setPerson] = useState("Da");
+  const [person, setPerson] = useState("");
+  const [tags, setTags] = useState([{ id: "Thailand", text: "Thailand" }]);
 
-  const onSubmit = (text, amount, type, person) => {
+  const onSubmit = (text, amount, type, person, tags) => {
     const newTransaction = {
       id: Math.floor(Math.random() * 1000000),
       text,
       amount: parseInt(amount, 10),
       type: type,
       person: person,
+      tag: tags,
     };
 
     addTransaction(newTransaction);
 
     setText("");
     setAmount("");
+    setTags([]);
+    setPerson("");
+  };
+
+  const handleTagAddition = (tag) => {
+    setTags([...tags, tag]);
   };
 
   return (
@@ -36,10 +45,29 @@ export const Newtransaction = () => {
                   id="desc"
                   type="text"
                   placeholder="Description"
-                  className="block w-full pl-3 pr-10 py-2 rounded-lg border-gray-300 bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent my-1 shadow-md"
+                  className="fluent-textbox my-1"
                   value={text}
                   onChange={(e) => setText(e.target.value)}
                   required
+                />
+              </div>
+
+              <div>
+                <ReactTags
+                  tags={tags}
+                  handleAddition={handleTagAddition}
+                  allowNew={true}
+                  placeholder="Add a tag"
+                  classNames={{
+                    tagInputField: "fluent-textbox",
+                    tag: "fluent-bg-gradient bg-indigo-100 text-indigo-800 inline-flex items-center rounded-full py-1.5 px-2.5 mb-2 mr-2 my-1",
+                    remove:
+                      "ml-1 inline-block align-middle cursor-pointer opacity-50 hover:opacity-100",
+                    tagInput: "w-full focus:outline-none",
+                    suggestions:
+                      "flex flex-wrap absolute z-10 bg-white border rounded w-full py-2 mt-1",
+                    suggestionActive: "bg-indigo-100",
+                  }}
                 />
               </div>
 
@@ -48,14 +76,14 @@ export const Newtransaction = () => {
                   id="money"
                   type="number"
                   placeholder="Amount in INR"
-                  className="block w-full pl-3 pr-10 py-2 rounded-lg border-gray-300 bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent my-1 shadow-md"
+                  className="fluent-textbox my-1"
                   value={amount}
                   onChange={(e) => setAmount(e.target.value)}
                 />
               </div>
 
               <select
-                class="block appearance-none w-full bg-white border-gray-300 text-gray-900 py-2 pl-3 pr-10 rounded-lg shadow-md leading-tight focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent my-1"
+                className="block appearance-none w-full bg-white border-gray-300 text-gray-900 py-2 pl-3 pr-10 rounded-lg shadow-md leading-tight focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent my-1 dropdown-content menu p-2 shadow bg-base-100 rounded-box"
                 value={type}
                 onChange={(e) => setType(e.target.value)}
               >
@@ -90,8 +118,8 @@ export const Newtransaction = () => {
 
               <div className=" w-full my-1">
                 <button
-                  className="fluent-button"
-                  onClick={() => onSubmit(text, amount, type, person)}
+                  className="fluent-button w-full"
+                  onClick={() => onSubmit(text, amount, type, person, tags)}
                 >
                   Add
                 </button>
